14831a201c421b634ead73447e32adff385345fe
Akhil Wali <akhil.wali.10@gmail.com>
2019-09-28 14:14:43 +1200

    Load aliases in eshell

--- +os.el
+++ +os.el
@@ -1,5 +1,11 @@
 ;;; ~/.doom.d/+os.el -*- lexical-binding: t; -*-
 
+(when IS-LINUX
+  (setq shell-command-switch "-ic"))
+
+(after! eshell
+  (add-hook 'eshell-mode-hook #'+eshell--load-bash-aliases-in-eshell))
+
 (use-package! edit-server
   :if window-system
   :defer 2
