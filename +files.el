e5d6d5ea96e57b61ff993723dc75273e477aa360
Akhil Wali <akhil.wali.10@gmail.com>
2019-09-30 12:11:19 +1300

    Add PDF settings

--- +files.el
+++ +files.el
@@ -4,3 +4,7 @@
   :mode ("\\.epub\\'" . nov-mode)
   :config
   (setq nov-save-place-file (concat doom-cache-dir "nov-places")))
+
+(after! pdf-tools
+  (setq-default pdf-view-display-size 'fit-width)
+  (add-hook! 'pdf-view-mode-hook #'pdf-view-midnight-minor-mode))
