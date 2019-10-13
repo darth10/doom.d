8860373ed19df550656d4b893fddae366547c054
Akhil Wali <akhil.wali.10@gmail.com>
2019-10-13 15:19:25 +1300

    Add more info to left modeline segment

--- +ui.el
+++ +ui.el
@@ -34,7 +34,7 @@
 
   (doom-modeline-def-modeline 'main
     '(workspace-name window-number bar cur-mode matches buffer-info-simple buffer-position selection-info)
-    '(debug buffer-encoding major-mode process vcs checker))
+    '(debug misc-info persp-name input-method indent-info buffer-encoding major-mode process vcs checker))
 
   (doom-modeline-def-modeline 'helm
     '(bar helm-number helm-follow helm-prefix-argument)
