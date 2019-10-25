ef8efc01f90a0b3da0e6b87de76dc262dcc5fdc9
Akhil Wali <akhil.wali.10@gmail.com>
2019-10-25 21:43:03 +1300

    Remove kubernetes use-package! declaration

--- +os.el
+++ +os.el
@@ -16,13 +16,3 @@
   :defer 2
   :config
   (clipmon-mode-start))
-
-(use-package! kubernetes
-  :commands (kubernetes-overview)
-  :hook (kubernetes-mode . +kubernetes--popup-setup)
-  :bind (:map kubernetes-mode-map
-         ("N" . kubernetes-set-namespace))
-  :config
-  (defun +kubernetes--popup-setup ()
-    (magit-define-popup-action 'kubernetes-overview-popup
-      ?N "Set namespace" 'kubernetes-set-namespace ?c)))
