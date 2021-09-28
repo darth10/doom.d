9dacf78efadd7f12072e1933c7c34bfe3bea402c
Akhil Wali <akhil.wali.10@gmail.com>
2021-09-28 15:55:08 +1300

    Add some AWS SSO helper functions

--- autoload/os.el
+++ autoload/os.el
@@ -29,3 +29,23 @@
     (if (get-buffer "bash-aliases") (kill-buffer "bash-aliases"))
     (if (get-buffer "bash-errors") (kill-buffer "bash-errors"))
     (message "Loaded aliases.")))
+
+;;;###autoload
+(defun +aws-list-profiles ()
+  (delete "default"
+          (split-string
+           (shell-command-to-string "aws configure list-profiles"))))
+
+;;;###autoload
+(defun +aws-sso-login ()
+  (interactive)
+  (let* ((choices (+aws-list-profiles))
+         (profile (completing-read "Profile: " choices)))
+    (async-shell-command (concat "aws sso login --profile " profile))))
+
+;;;###autoload
+(defun +aws-assume-role ()
+  (interactive)
+  (let* ((choices (+aws-list-profiles))
+         (profile (completing-read "Profile: " choices)))
+    (async-shell-command (concat "assume-aws " profile))))
