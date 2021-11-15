5400078d8f943ee01b38a2fe9704a627d59009bf
Akhil Wali <akhil.wali.10@gmail.com>
2021-11-16 11:16:52 +1300

    [magit] Skip diff while committing

--- +vc.el
+++ +vc.el
@@ -1,5 +1,8 @@
 ;;; ~/.doom.d/+vc.el -*- lexical-binding: t; -*-
 
+(after! magit
+  (setq magit-commit-show-diff nil))
+
 (after! forge
 ;; forge-pull-notifications fails for a large number of notifications
   (setq forge-pull-notifications nil)
