d30e40a2eef9627c6f350c9569facc67e20be3a0
Akhil Wali <akhil.wali.10@gmail.com>
2023-02-17 14:28:04 +1300

    Move logo text in *scratch* to `resources/` dir
    Also, change faces of initial scratch text.

--- autoload/ui.el
+++ autoload/ui.el
@@ -1,15 +1,5 @@
 ;;; ~/.doom.d/autoload/ui.el -*- lexical-binding: t; -*-
 
-(defconst +ui--scratch-message-logo-text
-  "
-    ____  ____  ____  __  ___   ________  ______   ___________
-   / __ \\/ __ \\/ __ \\/  |/  /  / ____/  |/  /   | / ____/ ___/
-  / / / / / / / / / / /|_/ /  / __/ / /|_/ / /| |/ /    \\__ \\
- / /_/ / /_/ / /_/ / /  / /  / /___/ /  / / ___ / /___ ___/ /
-/_____/\\____/\\____/_/  /_/  /_____/_/  /_/_/  |_\\____//____/
-
-")
-
 (defconst +ui--scratch-message-help-text
   "To open a file, type C-x C-f.
 To quit a partially entered command, type C-g.
@@ -23,8 +13,12 @@ For information about GNU Emacs and the GNU system, type C-h C-a.")
 ;;;###autoload
 (defun +ui--get-scratch-message ()
   "Get message to show in *scratch* buffer."
-  (let* ((face-for-logo 'font-lock-function-name-face)
-         (face-for-keys 'font-lock-string-face)
+  ;; '((t (:inherit font-lock-keyword-face :weight bold)))
+  (let* ((face-for-logo 'font-lock-keyword-face)
+         (face-for-keys 'font-lock-function-name-face)
+         (logo-text (with-temp-buffer
+                      (insert-file-contents (expand-file-name "resources/doomemacs.txt" doom-private-dir))
+                      (buffer-string)))
          (face-for-comments 'font-lock-comment-delimiter-face)
          (version-text (concat
                         (propertize (format "doom %s"
@@ -42,7 +36,7 @@ For information about GNU Emacs and the GNU system, type C-h C-a.")
     (concat
      (replace-regexp-in-string
       "^" (propertize ";; " 'face face-for-comments)
-      (propertize +ui--scratch-message-logo-text 'face face-for-logo))
+      (propertize (concat logo-text "\n") 'face face-for-logo))
      (replace-regexp-in-string
       "^" (propertize ";; " 'face face-for-comments)
       help-text)
