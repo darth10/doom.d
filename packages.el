254ac1c3af07641cc6cb94b090115c9a6604b9c9
Akhil Wali <akhil.wali.10@gmail.com>
2023-02-17 14:29:29 +1300

    Pin transient and with-editor packages
    See #7078 (doomemacs).

--- packages.el
+++ packages.el
@@ -24,3 +24,6 @@
 
 (when IS-WINDOWS
   (package! forge :disable t))
+
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
