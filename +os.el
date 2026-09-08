31c895cc37c6c2a32c54ff296735fb78ddf68ccc
Akhil Wali <akhil.wali.10@gmail.com>
2026-09-08 12:39:42 +1200

    Handle pass dirs that are symlinked

--- +os.el
+++ +os.el
@@ -16,9 +16,10 @@
 (after! pass
   (set-popup-rule! "^\\*Password-Store" :side 'left :size 0.4 :quit nil)
   (after! recentf
-    (dolist (dir (list (expand-file-name (password-store-dir))
-                       (abbreviate-file-name (password-store-dir))))
-      (add-to-list 'recentf-exclude (concat "\\`" (regexp-quote dir))))))
+    (let* ((resolved-password-store-dir (file-truename (password-store-dir))))
+      (dolist (dir (list (expand-file-name resolved-password-store-dir)
+                         (abbreviate-file-name resolved-password-store-dir)))
+        (add-to-list 'recentf-exclude (concat "\\`" (regexp-quote dir)))))))
 
 (after! plstore
   (after! epa
